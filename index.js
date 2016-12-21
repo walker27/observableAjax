6b588e7811cce3b81a78e1af3d89ef1ca85a3b36
wa1k3r <wa1k3r@192.168.2.154>
2016-12-22 00:48:02 +0800

    add usable demo

---
+++ index.js
@@ -0,0 +1,18 @@
+let ins = new ObservableAjax(function(params, successCb, errorCb){
+    $.ajax({
+        type: 'GET',
+        url: 'https://hacker-news.firebaseio.com/v0/item/8863.json',
+        data:{
+            print: 'pretty'
+        },
+        success: successCb,
+        error: errorCb
+    })
+});
+
+ins
+    .subscribe('this', function(data){
+        console.log('triggered');
+        console.log(data);
+    })
+    .update();
